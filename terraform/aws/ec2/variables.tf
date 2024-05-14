358227eb5f52b59953ff029267dfb0449f5b4422
tarikbaki <tarikbaki@hotmail.com>
2024-05-14 17:10:35 +0300

    First commit create tf files vpc etc

---
+++ terraform/aws/ec2/variables.tf
@@ -0,0 +1,12 @@
+# AWS Region
+variable "region" {
+  description = "Region in which AWS Resources to be created"
+  type        = string
+  default     = "us-east-1"
+}
+# Environment Variable
+variable "environment" {
+  description = "Environment Variable used as a prefix"
+  type        = string
+  default     = "prod"
+  }
